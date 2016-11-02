2be3d93d965e479e9417882ffc6e15ffffe7da46
YukitoshiM <amasihsotikuy@hotmail.co.jp>
2016-11-02 22:54:44 +0900

    Start writing TF-IDF, I am not sure yet!

--- reader.py
+++ reader.py
@@ -3,10 +3,12 @@
 import sys
 import MeCab as Mecab
 from collections import defaultdict
+import math
 
 # class for reading Tweeter data for any given file 
 class TweetReader():
    def __init__(self):
+      self.tagger = Mecab.Tagger('-Ochasen')
       # These list prepared for data in the Tweeter, 
       # First 4 variables prepared for a line which is devided by tab space
       # Last 3 are for contexts that is devided by space
@@ -14,7 +16,8 @@ class TweetReader():
       # reps:     contains for whom the tweet was replied 
       # texts:    contains actual tweet message
       self.ids, self.dates, self.contexts, self.unk_nums, self.rt_flags, self.reps, self.texts, self.lines, self.morpho= [], [], [], [], [], [], [], [], []
-      self.noun_count, self.adj_count = defaultdict(int), {}
+      self.noun_count = defaultdict(int)
+      self.total_words = 0
 
    # This method read lines in the input file and store it into class variable called self.lines
    def read_lines(self, filename):
@@ -24,6 +27,7 @@ class TweetReader():
          if line == '\n':
             self.lines.pop(i)
 
+   # This method get ids in self.lines and store it into class variable called self.ids
    def getIDs(self):
       for line in self.lines:
          temp_split = line.split('\t')
@@ -36,6 +40,7 @@ class TweetReader():
          elif len(temp_split) == 4:
             self.ids.append( temp_split[0] )
 
+   # This method get date info. in self.lines and store it into class variable called self.dates
    def getDates(self):
       for line in self.lines:
          temp_split = line.split('\t')
@@ -48,6 +53,7 @@ class TweetReader():
          elif len(temp_split) == 4:
             self.dates.append( temp_split[1] )
 
+   # This method get contexts info. in self.lines and store it into class variable called self.contexts
    def getContexts(self):
       for line in self.lines:
          temp_split = line.split('\t')
@@ -98,21 +104,60 @@ class TweetReader():
             self.unk_nums.append( None )
          elif len(temp_split) == 4:
             self.unk_nums.append( temp_split[3] )
-  
+   
+   # This method counts num of each noun in self.texts and store into self.noun_count
+   # e.g. noun_count = { noun1:4, noun2:5, noun3:10, .....}
    def noun_counter(self):
-      tagger = Mecab.Tagger('-Ochasen')
       for counter, text in enumerate(self.texts):
          counter += 1
          if (counter % 100000) == 0:
             print(counter)
          if type(text) != type(None):
-            for elem in tagger.parse(text.replace(' ','')).split('\n'):
+            for elem in self.tagger.parse(text.replace(' ','')).split('\n'):
                elem = elem.split('\t')
                try:
                   if elem[3][0:2] == '名詞':
                      self.noun_count[elem[0]] = self.noun_count.get(elem[0], 0) + 1
+                     self.total_words += 1
                except(IndexError):
                   continue
+
+   def getTF(self, word, text):
+      numOfWords = 0
+      for elem in self.tagger.parse(text.replace(' ','')).split('\n'):
+         elem = elem.split('\t')
+         if elem[0] == word:
+            numOfWords += 1
+      return numOfWords / total_words
+
+   def getDF(self, word):
+      numOfText = 0
+      word_flag = False
+      for counter, text in enumerate(self.texts):
+         counter += 1
+         if (counter % 100000) == 0:
+            print(counter)
+         if type(text) != type(None):
+            for elem in self.tagger.parse(text.replace(' ','')).split('\n'):
+               elem = elem.split('\t')
+               if elem[0] == word:
+                  word_flag = True
+         if word_flag:
+            numOfText += 1
+         word_flag = False
+      return numOfText
+
+   def getIDF(self, word):
+      df = self.getDF(word)
+      n = len(self.contexts)
+      return math.log2(n/df)
+
+   def getTFIDF(self, word, text):
+      tf = self.getTF(word, text)
+      idf = self.getIDF(word)
+      return tf*idf
+
+      
 
 
 """   def create_lists(self, filename):
@@ -188,3 +233,4 @@ if __name__=='__main__':
    reader.read_lines(sys.argv[1])
    reader.getContexts()
    reader.noun_counter()
+   print(reader.noun_count)
