1a94ae8a47391f9f600df3c70ba4b5e6d4d9d79d
YukitoshiM <amasihsotikuy@hotmail.co.jp>
2016-11-04 23:27:16 +0900

    Added Regular Expression Method for eliminating alphabet strings from noun_count!

--- reader.py
+++ reader.py
@@ -1,5 +1,5 @@
 # This program reads crowled Tweeter data
-
+import re
 import sys
 import MeCab as Mecab
 from collections import defaultdict
@@ -9,6 +9,7 @@ import math
 class TweetReader():
    def __init__(self):
       self.tagger = Mecab.Tagger('-Ochasen')
+      self.alnum_Reg = re.compile(r'^[a-zA-Z0-9_]+$')
       # These list prepared for data in the Tweeter, 
       # First 4 variables prepared for a line which is devided by tab space
       # Last 3 are for contexts that is devided by space
@@ -104,6 +105,9 @@ class TweetReader():
             self.unk_nums.append( None )
          elif len(temp_split) == 4:
             self.unk_nums.append( temp_split[3] )
+
+   def isalnum_(self, s):
+       return self.alnum_Reg.match(s) is not None
    
    # This method counts num of each noun in self.texts and store into self.noun_count
    # e.g. noun_count = { noun1:4, noun2:5, noun3:10, .....}
@@ -117,8 +121,9 @@ class TweetReader():
                elem = elem.split('\t')
                try:
                   if elem[3][0:2] == '名詞':
-                     self.noun_count[elem[0]] = self.noun_count.get(elem[0], 0) + 1
-                     self.total_words += 1
+                     if not (self.isalnum_(elem[0])):
+                        self.noun_count[elem[0]] = self.noun_count.get(elem[0], 0) + 1
+                        self.total_words += 1
                except(IndexError):
                   continue
 
@@ -156,77 +161,6 @@ class TweetReader():
       tf = self.getTF(word, text)
       idf = self.getIDF(word)
       return tf*idf
-
-      
-
-
-"""   def create_lists(self, filename):
-      # reads files and save lines in to a list
-      with open(filename, 'r') as f:
-         lines = f.readlines()
-
-      # This line assignes empty lists if the method is called more than twice
-      self.ids, self.dates, self.contexts, self.unk_nums, self.rt_flags, self.reps, self.texts = [], [], [], [], [], [], []
-
-      # for each line of file
-      for line in lines:
-         # this if statement ignore only if the lien is new line(sometime, this happen)
-         if line == '\n':
-            continue
-         # Firstable, devide line by tab sapace, and maximum list size is 4
-         # For some line, lists' size is less than 4
-         # 1st case: contains only message(context)
-         # 2nd case: contains messages and unknown numbers
-         # 3rd case: missing unknown numbers
-         temp_split = line.split('\t')
-         if len(temp_split) == 1:
-            self.ids.append( None )
-            self.dates.append( None )
-            self.contexts.append( temp_split[0] )
-            self.unk_nums.append( None )
-         elif len(temp_split) == 2:
-            self.ids.append( None )
-            self.dates.append( None )
-            self.contexts.append( temp_split[0] )
-            self.unk_nums.append( temp_split[1] )
-         elif len(temp_split) == 3:
-            self.ids.append( temp_split[0] )
-            self.dates.append( temp_split[1] )
-            self.contexts.append( temp_split[2] )
-            self.unk_nums.append( None )
-         elif len(temp_split) == 4:
-            self.ids.append( temp_split[0] )
-            self.dates.append( temp_split[1] )
-            self.contexts.append( temp_split[2])
-            self.unk_nums.append( temp_split[3] )
-      
-      # Since tweet message contains RT, @{name}, & actual message, messages are devided
-      for context in self.contexts:
-         # 1st case: contains 'RT' & @{name} & actual message
-         # 2nd case: contains @{name} & actual message
-         # 3rd case: contains only actual message
-         if context[0:2] == 'RT':
-            context = context.split()
-            self.rt_flags.append(1)
-            # Some message has 'RT' at the beggining of contex, but message is something like 'RT>fdsafsdf'
-            # so this will be ignored! 
-            try:
-               self.reps.append(context[1])
-               self.texts.append(' '.join(context[2:]))
-            except:
-               self.rt_flags.append(0)
-               self.reps.append(None)
-               self.texts.append(None)
-               continue
-         elif context[0] == '@':
-            context = context.split()
-            self.rt_flags.append(0)
-            self.reps.append(context[0])
-            self.texts.append(' '.join(context[1:]))
-         else:
-            self.rt_flags.append(0)
-            self.reps.append(None)
-            self.texts.append(context)"""
 
 if __name__=='__main__':
    reader = TweetReader()
